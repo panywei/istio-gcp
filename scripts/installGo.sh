dafdb19d6532b7bd729448e3ccd47695455dbc51
Yanwei Pan <yanwei.pan@gmail.com>
2018-03-01 16:45:38 -0500

    fix wrong arg

--- scripts/installGo.sh
+++ scripts/installGo.sh
@@ -1,7 +1,7 @@
 VERSION=1.10.linux-amd64
 GOROOTPARENT=/usr/local/
 wget https://storage.googleapis.com/golang/go${VERSION}.tar.gz
-sudo tar -cvzpf go${VERSION}.tar.gz ${GOROOTPARENT}
+sudo tar -zxf go${VERSION}.tar.gz -C ${GOROOTPARENT}
 
 printf "\n%s\n%s\n%s\n" \
   "export GOROOT=/usr/local/go" \
