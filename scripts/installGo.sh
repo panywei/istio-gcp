f0e815e7901e8b4ea8fa6851db9ce5b5c1087fa1
Yanwei Pan <yanwei.pan@gmail.com>
2018-03-01 16:27:58 -0500

    add GO PATH to .bashrc

--- scripts/installGo.sh
+++ scripts/installGo.sh
@@ -3,3 +3,10 @@ GOROOTPARENT=/usr/local/
 wget https://storage.googleapis.com/golang/go${VERSION}.tar.gz
 tar -xvf go${VERSION}.tar.gz
 sudo mv go ${GOROOTPARENT}
+
+printf "\n%s\n%s\n%s\n" \ 
+  "export GOROOT=/usr/local/go" \ 
+  "export GOPATH=~/go" \
+  "export PATH=\$GOPATH/bin:\$GOROOT/bin:\$PATH" \
+  >> ~/.bashrc
+  
