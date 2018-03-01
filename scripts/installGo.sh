423c6962842c6e286e9865e2d7c403732772b5b5
Yanwei Pan <yanwei.pan@gmail.com>
2018-03-01 16:40:24 -0500

    extract to GOROOT directly

--- scripts/installGo.sh
+++ scripts/installGo.sh
@@ -1,8 +1,7 @@
 VERSION=1.10.linux-amd64
 GOROOTPARENT=/usr/local/
 wget https://storage.googleapis.com/golang/go${VERSION}.tar.gz
-tar -xvf go${VERSION}.tar.gz
-sudo mv go ${GOROOTPARENT}
+sudo tar -cvzpf go${VERSION}.tar.gz ${GOROOTPARENT}
 
 printf "\n%s\n%s\n%s\n" \
   "export GOROOT=/usr/local/go" \
